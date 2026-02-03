5e4ad9cef67c29ece135055d007bcdb1045136af
RaufCode <your_email@example.com>
2026-02-03 11:33:47 +0000

    refactor index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import express from "express"
-import bcrypt from "bcrypt"
 import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .type("html")
-    .send("<h1>MyBlog</h1><p>Backend is running</p>")
-})
-
+app.use(express.json())
 
 
 
